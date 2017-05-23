6a1ee11ffc821cdd97fdd55eb2eb7b1434dc2771
Caleb Tote <ctote@ea.com>
2017-05-23 15:09:39 -0400

    Updating port settings to work with Heroku

--- app.js
+++ app.js
@@ -4,13 +4,18 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+
 app.use(express.static(__dirname + '/'));
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/devotions.html'));
 })
 
-app.listen(3000, function () {
-    console.log('Duki.space listening on port 3000!')
+app.listen(port, function () {
+    console.log('Duki.space listening on port ' + port);
 })
 
